a63ad6e5a037ac139ddccef907df8d363d8b034a
Tanakorn Paiboonwattanapon <tanakorn.nonlp@gmail.com>
2018-07-07 09:52:34 +0700

    fist

---
+++ app/config/index.js
@@ -0,0 +1,16 @@
+const app = {
+  db: {
+    host: "ds259855.mlab.com",
+    port: 59855,
+    name: "social",
+    username: "admin",
+    password: "Wavei5795"
+  },
+  jwt: {
+    APP_SECRET: "ILOVEYOU8",
+    EXPIRE_IN: "7d"
+  }
+};
+
+export const database = app.db;
+export default app;
